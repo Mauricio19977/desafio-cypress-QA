7be2e00d072633671d1a5ddc8061f938770604d4
Mauricio19977 <85204684+Mauricio19977@users.noreply.github.com>
2025-05-22 10:55:09 -0300

    Update homeElements.js

--- cypress/e2e/Elements/homeElements.js
+++ cypress/e2e/Elements/homeElements.js
@@ -2,7 +2,8 @@ export const HomeElements = {
 
     validateUser: ('i[class="icon-login icons"]'),
     buttonBuy: ('i[class="icon-earphones-alt icons"]'),
-    buttonLogout: ('.topbar-inner > :nth-child(1) > .list-inline > :nth-child(2) > a'),
+    buttonLogout: ('.topbar-inner > :nth-child(1) > .list-inline > :nth-child(2) > a')
+    
 }
 
 export default HomeElements; 
