9f070867bd3102d7b677236e97b6fd10f1d8a313
Mauricio19977 <85204684+Mauricio19977@users.noreply.github.com>
2025-05-22 11:00:50 -0300

    Update cart.cy.js

--- cypress/e2e/Executions/cart.cy.js
+++ cypress/e2e/Executions/cart.cy.js
@@ -4,17 +4,22 @@ import Users from "../../fixtures/users";
 import ProductElements from "../Elements/productElements";
 import HomeElements from "../Elements/homeElements";
 import CartElements from "../Elements/cartElements";
+
 describe('suite de testes executando cenarios realacionado ai carrinho', () => {
     
-    beforeEach('', () => {
+    beforeEach('Realiza a abertura da página da aplicação e o login', () => {
+        
         Methods.aberturaDeSite();
         Methods.click(LoginElements.buttonLogin)
         Methods.inputText(LoginElements.inputUser, Users.usuarioMauricio)
         Methods.inputText(LoginElements.inputPassword, Users.senhaMauricio)
         Methods.click(LoginElements.buttonEnter)
         Methods.validateElementPresent(HomeElementos.validateUser)
+        
     });
+    
     it('Adicionar um produto ao carrinho', () => {
+        
         Methods.clickElement(HomeElementos.buttonBuy)
         Methods.clickElement(ProductElements.btnImg)
         Methods.click(ProductElements.buttonM)
@@ -23,8 +28,11 @@ describe('suite de testes executando cenarios realacionado ai carrinho', () => {
         Methods.validateElementPresent(ProductElements.buttonCart)
         Methods.click(ProductElements.buttonCart)
         Methods.validateElementPresent(CartElements.buttonCoupon)
+        
     });
-    it.only('Adicionar um produto ao carrinho e excluir o produto', () => {
+    
+    it('Adicionar um produto ao carrinho e excluir o produto', () => {
+        
         Methods.clickElement(HomeElements.buttonBuy)
         Methods.clickElement(ProductElements.btnImg)
         Methods.click(ProductElements.buttonM)
@@ -35,5 +43,7 @@ describe('suite de testes executando cenarios realacionado ai carrinho', () => {
         Methods.validateElementPresent(CartElements.buttonCoupon)
         Methods.click(CartElements.buttonDeleteProduct)
         Methods.validateElementPresent(CartElements.buttonReturnStore)
- });
-})
+        
+   })
+    
+});
