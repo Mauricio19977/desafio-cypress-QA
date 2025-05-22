3ade5f8c0a5079e0852ecdbaa91870f2a3481d5c
Mauricio19977 <85204684+Mauricio19977@users.noreply.github.com>
2025-05-22 11:03:41 -0300

    Update login.cy.js

--- cypress/e2e/Executions/login.cy.js
+++ cypress/e2e/Executions/login.cy.js
@@ -2,23 +2,33 @@ import Methods from "../Methods/methods";
 import LoginElements from "../Elements/loginElements";
 import Users from "../../fixtures/users";
 import HomeElements from "../Elements/homeElements";
+
 describe("suit de testes executando cenarios de login", () => {
-  beforeEach("", () => {
+  
+  beforeEach("Realização de abertura da aplicação", () => {
     
-    Methods.aberturaDeSite();
-  });
+    Methods.aberturaDeSite()
+    
+  })
+  
   it("Login com sucesso", () => {
-    Methods.click(LoginElements.buttonLogin);
-    Methods.inputText(LoginElements.inputUser, Users.userName);
-    Methods.inputText(LoginElements.inputPassword, Users.passaword);
+    
+    Methods.click(LoginElements.buttonLogin)
+    Methods.inputText(LoginElements.inputUser, Users.userName)
+    Methods.inputText(LoginElements.inputPassword, Users.passaword)
     Methods.click(LoginElements.buttonEnter);
-    Methods.validateElementPresent(HomeElements.validateUser);
-  });
-  it("Login incorreto", () => {
-    Methods.click(LoginElements.buttonLogin);
-    Methods.inputText(LoginElements.inputUser, Users.userName);
-    Methods.inputText(LoginElements.inputPassword, Users.passaword);
-    Methods.click(LoginElements.buttonEnter);
-    Methods.validateElementPresent(LoginElements.buttonEnter);
-  });
+    Methods.validateElementPresent(HomeElements.validateUser)
+    
+  })
+  
+  it("Login com a senha incorreta", () => {
+    
+    Methods.click(LoginElements.buttonLogin)
+    Methods.inputText(LoginElements.inputUser, Users.userName)
+    Methods.inputText(LoginElements.inputPassword, Users.passaword)
+    Methods.click(LoginElements.buttonEnter)
+    Methods.validateElementPresent(LoginElements.buttonEnter)
+    
+  })
+  
 });
