39b182ed936104d81471d974857d242930d22c10
Mauricio19977 <85204684+Mauricio19977@users.noreply.github.com>
2025-05-22 10:56:14 -0300

    Update loginElements.js

--- cypress/e2e/Elements/loginElements.js
+++ cypress/e2e/Elements/loginElements.js
@@ -3,7 +3,8 @@ export const LoginElements = {
     buttonLogin: ('i[class="icon-user-unfollow icons"]'),
     inputUser: ('input[id="username"]'),
     inputPassword: ('input[id="password"]'),
-    buttonEnter: ('input[name="login"]'),
+    buttonEnter: ('input[name="login"]')
+    
 }
 
 export default LoginElements; 
