5acf088de2123788d68c5783486b27ca02d750f9
Mauricio19977 <85204684+Mauricio19977@users.noreply.github.com>
2025-05-22 11:06:10 -0300

    Update users.js

--- cypress/fixtures/users.js
+++ cypress/fixtures/users.js
@@ -1,7 +1,9 @@
 export const Users = {
+  
   userName: "soares_mauricio@outlook.com",
   passaword: "desafio123",
-  passawordIncorrect: "desafio1234",
+  passawordIncorrect: "desafio1234"
+  
 };
 
 export default Users;
