cdadf08cce27ce188d24e902304983ead8416cac
Mauricio19977 <85204684+Mauricio19977@users.noreply.github.com>
2025-05-22 11:04:56 -0300

    Update logout.cy.js

--- cypress/e2e/Executions/logout.cy.js
+++ cypress/e2e/Executions/logout.cy.js
@@ -2,18 +2,24 @@ import Methods from "../Methods/methods";
 import LoginElements from "../Elements/loginElements";
 import Users from "../../fixtures/users";
 import HomeElements from "../Elements/homeElements";
+
 describe("suit de testes executando cenarios de login", () => {
 
-  beforeEach("", () => {
+  beforeEach("Realização de abertura da aplicação", () => {
     
-    Methods.aberturaDeSite();
-  });
+    Methods.aberturaDeSite()
+    
+  })
+  
   it("Logout com sucesso", () => {
-    Methods.click(LoginElements.buttonLogin);
-    Methods.inputText(LoginElements.inputUser, Users.userName);
-    Methods.inputText(LoginElements.inputPassword, Users.passaword);
-    Methods.click(LoginElements.buttonEnter);
-    Methods.click(HomeElements.buttonLogout);
-    Methods.validateElementPresent(LoginElements.buttonLogin);
-  });
+    
+    Methods.click(LoginElements.buttonLogin)
+    Methods.inputText(LoginElements.inputUser, Users.userName)
+    Methods.inputText(LoginElements.inputPassword, Users.passaword)
+    Methods.click(LoginElements.buttonEnter)
+    Methods.click(HomeElements.buttonLogout)
+    Methods.validateElementPresent(LoginElements.buttonLogin)
+    
+  })
+  
 });
